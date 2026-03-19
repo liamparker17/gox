f0a8b90d08785b8f8eafe2e7f058074411319961
Liam Parker <liamparker17@gmail.com>
2026-03-20 01:42:10 +0200

    feat: parser for func blocks and match expressions

--- parser/func.go
+++ parser/func.go
@@ -1,9 +1,61 @@
 package parser
 
-import "github.com/liamp/gox/ast"
+import (
+	"strings"
+
+	"github.com/liamp/gox/ast"
+	"github.com/liamp/gox/lexer"
+)
 
 func (p *Parser) parseFunc() *ast.Func {
-	p.next()
-	p.errorf("func parsing not yet implemented")
-	return &ast.Func{}
+	pos := p.tok.Pos
+	var sigParts []string
+	for p.tok.Kind != lexer.TokLBrace && p.tok.Kind != lexer.TokEOF {
+		sigParts = append(sigParts, p.tok.Value)
+		p.next()
+	}
+	p.expect(lexer.TokLBrace)
+	fn := &ast.Func{Position: pos, Signature: strings.Join(sigParts, " ")}
+	fn.Stmts = p.parseFuncBody()
+	return fn
+}
+
+func (p *Parser) parseFuncBody() []ast.Stmt {
+	var stmts []ast.Stmt
+	depth := 1
+	var goCode strings.Builder
+	for p.tok.Kind != lexer.TokEOF {
+		if p.tok.Kind == lexer.TokRBrace {
+			depth--
+			if depth == 0 {
+				p.next()
+				break
+			}
+			goCode.WriteString("}")
+			p.next()
+			continue
+		}
+		if p.tok.Kind == lexer.TokMatch && depth == 1 {
+			if code := strings.TrimSpace(goCode.String()); code != "" {
+				stmts = append(stmts, &ast.GoCode{Code: code})
+				goCode.Reset()
+			}
+			m := p.parseMatchExpr()
+			stmts = append(stmts, m)
+			continue
+		}
+		if p.tok.Kind == lexer.TokLBrace {
+			depth++
+			goCode.WriteString("{")
+			p.next()
+			continue
+		}
+		goCode.WriteString(p.tok.Value)
+		goCode.WriteString(" ")
+		p.next()
+	}
+	if code := strings.TrimSpace(goCode.String()); code != "" {
+		stmts = append(stmts, &ast.GoCode{Code: code})
+	}
+	return stmts
 }
---
+++ parser/match.go
@@ -0,0 +1,104 @@
+package parser
+
+import (
+	"strings"
+
+	"github.com/liamp/gox/ast"
+	"github.com/liamp/gox/lexer"
+)
+
+func (p *Parser) parseMatchExpr() *ast.Match {
+	pos := p.tok.Pos
+	p.next() // skip "match"
+	expr := p.expect(lexer.TokIdent)
+	p.expect(lexer.TokColon)
+	typeName := p.expect(lexer.TokIdent)
+	p.expect(lexer.TokLBrace)
+	m := &ast.Match{
+		Position: pos,
+		Expr:     expr.Value,
+		TypeName: typeName.Value,
+		Ignore:   p.lastDirective == "ignore-exhaustive",
+	}
+	p.lastDirective = ""
+	for p.tok.Kind != lexer.TokRBrace && p.tok.Kind != lexer.TokEOF {
+		arm := p.parseMatchArm()
+		m.Arms = append(m.Arms, arm)
+	}
+	p.expect(lexer.TokRBrace)
+	return m
+}
+
+func (p *Parser) parseMatchArm() ast.MatchArm {
+	variant := p.expect(lexer.TokIdent)
+	arm := ast.MatchArm{Variant: variant.Value}
+	if p.tok.Kind == lexer.TokLParen {
+		p.next()
+		for p.tok.Kind != lexer.TokRParen && p.tok.Kind != lexer.TokEOF {
+			if p.tok.Kind == lexer.TokIdent {
+				arm.Bindings = append(arm.Bindings, p.tok.Value)
+			}
+			if p.tok.Kind == lexer.TokComma {
+				p.next()
+				continue
+			}
+			p.next()
+		}
+		p.expect(lexer.TokRParen)
+	}
+	p.expect(lexer.TokArrow)
+	var body strings.Builder
+	depth := 0
+	for p.tok.Kind != lexer.TokEOF {
+		if depth == 0 && p.tok.Kind == lexer.TokRBrace {
+			break
+		}
+		if depth == 0 && p.tok.Kind == lexer.TokIdent {
+			if p.peekIsArmStart() {
+				break
+			}
+		}
+		if p.tok.Kind == lexer.TokLBrace {
+			depth++
+		}
+		if p.tok.Kind == lexer.TokRBrace {
+			depth--
+		}
+		body.WriteString(p.tok.Value)
+		body.WriteString(" ")
+		p.next()
+	}
+	arm.Body = strings.TrimSpace(body.String())
+	return arm
+}
+
+func (p *Parser) peekIsArmStart() bool {
+	savedLex := *p.lex
+	savedTok := p.tok
+	p.next()
+	isArm := false
+	if p.tok.Kind == lexer.TokArrow {
+		isArm = true
+	} else if p.tok.Kind == lexer.TokLParen {
+		parenDepth := 1
+		p.next()
+		for parenDepth > 0 && p.tok.Kind != lexer.TokEOF {
+			if p.tok.Kind == lexer.TokLParen {
+				parenDepth++
+			}
+			if p.tok.Kind == lexer.TokRParen {
+				parenDepth--
+			}
+			if parenDepth > 0 {
+				p.next()
+			}
+		}
+		if parenDepth == 0 {
+			p.next()
+			isArm = p.tok.Kind == lexer.TokArrow
+		}
+	}
+	*p.lex = savedLex
+	p.tok = savedTok
+	return isArm
+}
